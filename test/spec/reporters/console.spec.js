a19ca896f3fa306caebd2ba2cc0f5eac4b91bb60
Joel Chen <xchen@walmartlabs.com>
2017-05-29 23:11:10 -0700

    tweak timestamp and indent colors

--- test/spec/reporters/console.spec.js
+++ test/spec/reporters/console.spec.js
@@ -4,8 +4,12 @@ const XReporterConsole = require("../../../lib/reporters/console");
 const XClap = require("../../../lib/xclap");
 const XQItem = require("../../../lib/xqitem");
 const expect = require("chai").expect;
+const chalk = require("chalk");
 
 describe("XReporterConsole", function () {
+  const saved = chalk.enabled;
+  before(() => chalk.enabled = false);
+  after(() => chalk.enabled = saved);
   it("should indent by qitem level", () => {
     const xclap = new XClap();
     const reporter = new XReporterConsole(xclap);
